1ea64618999aebf0da5af249254444d1db106c85
Memory Clutter <memclutter@gmail.com>
2022-11-09 23:00:21 +0300

    Added test for invalid status code

--- request_test.go
+++ request_test.go
@@ -27,7 +27,26 @@ func TestProxyRequest(t *testing.T) {
 
 	body, err := ProxyRequest(targetURLEnv, proxyURL, timeout)
 	require.NoError(t, err, "must be success proxy request")
-	assert.Contains(t, fmt.Sprintf("%s", body), "Welcome to nginx!", "must be contain REQUEST_URI")
+	assert.Contains(t, fmt.Sprintf("%s", body), "Welcome to nginx!", "must be contain Welcome to nginx!")
+}
+
+func TestProxyRequestNot200StatusCode(t *testing.T) {
+	proxyURLEnv := os.Getenv("PROXY_URL")
+	targetURLEnv := os.Getenv("TARGET_URL")
+	if len(proxyURLEnv) == 0 {
+		t.Skip("Skipped because PROXY_URL not set")
+		return
+	} else if len(targetURLEnv) == 0 {
+		t.Skip("Skipped because TARGET_URL not set")
+		return
+	}
+
+	timeout := 10 * time.Second
+	proxyURL, err := url.Parse(proxyURLEnv)
+	require.NoError(t, err, "must be correct PROXY_URL url address")
+
+	_, err = ProxyRequest(targetURLEnv+"/not_found_page/", proxyURL, timeout)
+	require.ErrorContains(t, err, `http error, status code 404`, "must be contain error")
 }
 
 func TestProxyRequestNotReachableError(t *testing.T) {
