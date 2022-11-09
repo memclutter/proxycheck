da86ba3af0156d69ccf84a8ced38d02ab4654913
Memory Clutter <memclutter@gmail.com>
2022-11-09 22:50:55 +0300

    Use env var for target and service

--- request_test.go
+++ request_test.go
@@ -12,8 +12,12 @@ import (
 
 func TestProxyRequest(t *testing.T) {
 	proxyURLEnv := os.Getenv("PROXY_URL")
+	targetURLEnv := os.Getenv("TARGET_URL")
 	if len(proxyURLEnv) == 0 {
 		t.Skip("Skipped because PROXY_URL not set")
+		return
+	} else if len(targetURLEnv) == 0 {
+		t.Skip("Skipped because TARGET_URL not set")
 		return
 	}
 
@@ -21,7 +25,16 @@ func TestProxyRequest(t *testing.T) {
 	proxyURL, err := url.Parse(proxyURLEnv)
 	require.NoError(t, err, "must be correct PROXY_URL url address")
 
-	body, err := ProxyRequest("http://www.wfuchs.de/azenv.php", proxyURL, timeout)
+	body, err := ProxyRequest(targetURLEnv, proxyURL, timeout)
 	require.NoError(t, err, "must be success proxy request")
 	assert.Contains(t, fmt.Sprintf("%s", body), "REQUEST_URI = /azenv.php", "must be contain REQUEST_URI")
+}
+
+func TestProxyRequestNotReachableError(t *testing.T) {
+	timeout := 100 * time.Millisecond
+	proxyURL, err := url.Parse("http://localhost:1111")
+	require.NoError(t, err, "must be correct PROXY_URL url address")
+
+	_, err = ProxyRequest("http://localhost", proxyURL, timeout)
+	require.ErrorContains(t, err, `can't http get: Get "http://localhost": proxyconnect tcp: dial tcp 127.0.0.1:1111: connect: connection refused`, "must be error")
 }
