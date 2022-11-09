da9d3410485bdaf5f3ff2a64df354ba182eeab27
Memory Clutter <memclutter@gmail.com>
2022-11-09 21:51:03 +0300

    Tests for createProxytransport func

--- utils_test.go
+++ utils_test.go
@@ -5,7 +5,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
+	"net/url"
 	"testing"
+	"time"
 )
 
 func TestReadResponse(t *testing.T) {
@@ -15,4 +17,51 @@ func TestReadResponse(t *testing.T) {
 
 	assert.NoError(t, err, "must be ok")
 	assert.Equal(t, exceptedBody, actualBody, "must be body equal")
+}
+
+func TestCreateProxyTransport(t *testing.T) {
+	testingTable := []struct {
+		title    string
+		proxyURL *url.URL
+		timeout  time.Duration
+	}{
+		{
+			title: "socks4 proxy use correct http transport and timeout",
+			proxyURL: &url.URL{
+				Scheme: "socks4",
+				Host:   "100.200.100.20:500",
+			},
+			timeout: 4 * time.Second,
+		},
+		{
+			title: "http proxy use correct http transport and timeout",
+			proxyURL: &url.URL{
+				Scheme: "http",
+				Host:   "239.10.200.30",
+			},
+			timeout: 120 * time.Millisecond,
+		},
+	}
+
+	for _, table := range testingTable {
+		t.Run(table.title, func(t *testing.T) {
+			httpTransport := createProxyTransport(table.proxyURL, table.timeout)
+
+			assert.Equal(t, table.timeout, httpTransport.TLSHandshakeTimeout, "must be equal timeout")
+			assert.Equal(t, table.timeout, httpTransport.IdleConnTimeout, "must be equal timeout")
+			assert.Equal(t, table.timeout, httpTransport.ResponseHeaderTimeout, "must be equal timeout")
+			assert.True(t, httpTransport.DisableKeepAlives, "must be disable keep alive")
+			assert.False(t, httpTransport.DisableCompression, "must be enable compression")
+
+			assert.NotNil(t, httpTransport.DialContext, "must be set dialer")
+
+			// Only for non socks proxy, check proxy url correct
+			if table.proxyURL.Scheme == "http" || table.proxyURL.Scheme == "https" {
+				exceptedProxyURL, _ := http.ProxyURL(table.proxyURL)(nil)
+				actualProxyURL, _ := httpTransport.Proxy(nil)
+
+				assert.Equal(t, exceptedProxyURL, actualProxyURL)
+			}
+		})
+	}
 }
